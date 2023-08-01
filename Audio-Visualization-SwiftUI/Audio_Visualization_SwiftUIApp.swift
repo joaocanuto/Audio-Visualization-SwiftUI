b7b1c2ed46f2e691abdac07ac05527ea8ccc9c1e
João Guilherme <joaogacanuto@gmail.com>
2023-08-01 19:27:27 -0300

    Initial Commit

---
+++ Audio-Visualization-SwiftUI/Audio_Visualization_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_Visualization_SwiftUIApp.swift
+//  Audio-Visualization-SwiftUI
+//
+//  Created by Joao Guilherme Araujo Canuto on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Audio_Visualization_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
